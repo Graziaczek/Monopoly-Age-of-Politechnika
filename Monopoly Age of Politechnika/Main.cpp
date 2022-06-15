5d9658594d8cc421beb13be830be72a7cd12b926
Graziaczek <66276396+Graziaczek@users.noreply.github.com>
2022-06-15 12:53:10 +0200

    Tytuł v1

--- Monopoly Age of Politechnika/Main.cpp
+++ Monopoly Age of Politechnika/Main.cpp
@@ -1,15 +1,35 @@
 #include <SFML/Graphics.hpp>
 #include <SFML/Window.hpp>
+#include <iostream>
 
+using namespace sf;
+using namespace std;
 int main() {
-	sf::Window window(sf::VideoMode(1200, 800), "Monopoly: Age of Politechnika",sf::Style::Default);
+	RenderWindow window(VideoMode(1000, 800), "Monopoly: Age of Politechnika",Style::Default);
+	window.setFramerateLimit(60);
+	Texture tytul;
+	Texture nowagra;
+	tytul.loadFromFile("Nazwa.jpg");
+	nowagra.loadFromFile("NowaGra.jpg");
+	Sprite tytul_s;
+	Sprite nowagra_s;
+	tytul_s.setTexture(tytul);
+	tytul_s.setPosition(Vector2f(300,100));
+	nowagra_s.setTexture(nowagra);
+	nowagra_s.setPosition(Vector2f(375, 300));
+	nowagra_s.setScale(0.5,0.5);
 	while (window.isOpen()) {
-		sf::Event event;
+		Event event;
 		while (window.pollEvent(event)) {
-			if (event.type == sf::Event::Closed) {
+			if (event.type == Event::Closed) {
 				window.close();
 			}
 		}
+		window.clear(Color::Black);
+		window.draw(tytul_s);
+		window.draw(nowagra_s);
+		if ();
+		window.display();
 	}
 	return 0;
 }
